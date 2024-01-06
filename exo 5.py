417175c6c1e4aa590147fa284fba03e9d6e3f49c
Khaledsabane <148535782+Khaledsabane@users.noreply.github.com>
2024-01-06 14:09:27 +0100

    Update exo 5.py

--- exo 5.py
+++ exo 5.py
@@ -1,26 +1,23 @@
-def calcul_prix_location(heure_debut, heure_fin):
+debut_location = int(input("Donnez l’heure de début de la location : "))
+fin_location = int(input("Donnez l’heure de fin de la location  : "))
+somme_heures_1euro=0
+somme_heures_2euro=0
+if debut_location<0 or fin_location>24  :
+    print(f"Les heures doivent être comprises entre 0 et 24 ! ")
+elif debut_location==fin_location:
+    print(f"Attention ! l’heure de fin est identique à l’heure de début.")
+elif debut_location>fin_location :
+    print(f"Attention ! le début de la location est après la fin ... ")
+else:
+    for heure in range(debut_location,fin_location):
+        if heure < 7 or (heure >= 17 and heure <= 24):
+            somme_heures_1euro+=1
+        elif heure>=7 and heure<17:
+            somme_heures_2euro += 1
+    print(f"Vous avez loué votre vélo pendant")
+    if somme_heures_1euro>0:
+        print(f"{somme_heures_1euro} heure(s) au tarif horaire de 1.0 euro(s) ")
+    if somme_heures_2euro > 0:
+        print(f"{somme_heures_2euro} heure(s) au tarif horaire de 2.0 euro(s)")
 
-
-    if heure_debut < 0 or heure_fin > 24:
-        print("Les heures doivent être comprises entre 0 et 24 !\n")
-        return
-    if heure_debut == heure_fin:
-        print("Attention ! L'heure de fin est identique à l'heure de début.\n")
-        return
-    elif heure_debut > heure_fin:
-        print("Attention ! Le début de la location est après la fin.\n")
-        return
-
-
-    if heure_debut >= 0 and heure_fin <= 7 or heure_debut >= 17 and heure_fin <= 24:
-        prix = (heure_fin - heure_debut) * 1
-    else:
-        prix = (heure_fin - heure_debut) * 2
-
-    print(f"Le coût de la location est de {prix} euros.")
-
-heure_debut = int(input("Veuillez entrer l'heure de début de location : "))
-heure_fin = int(input("Veuillez entrer l'heure de fin de location : "))
-
-
-calcul_prix_location(heure_debut, heure_fin)
+    print(f"Le montant total à payer est de {(somme_heures_1euro+somme_heures_2euro*2)} euro(s)")
