d63f3b0f6c1c3f08502be1aa22ce5ed34afde522
Khaledsabane <148535782+Khaledsabane@users.noreply.github.com>
2024-01-06 13:26:27 +0100

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,5 +1,17 @@
-factorielle=1
-n = int(input("saisir un nombre :"))
-for i in range(1, n+1):
-    factorielle *=i
-print(f"le factorielle est:{factorielle}")
+n= int(input("entrez un nombre:"))
+produit=1
+nb=1
+bool=int(input("veillez choisir la boucle (tapez 1 pour for), (tapez 0 pour while) :"))
+if bool==1:
+    for i in range(1,n+1):
+        produit*=i
+        print(f"le produit est : {produit}")
+        if i==n:
+            print(f"la factorielle  est : {produit}")
+elif bool==0:
+    while nb!=n+1:
+        produit*=nb
+        print(f"le produit est : {produit}")
+        if nb == n:
+            print(f"la factorielle  est : {produit}")
+        nb+=1
